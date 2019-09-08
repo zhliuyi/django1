211845c2b0f2e8165adb3d70a1b84b4547347491
zhangsan <zhangsan@126.com>
2019-09-08 20:23:02 +0800

    day01 demo

---
+++ demo/demo/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    """
+    视图   函数视图
+    :param request:  请求对象，包含了请求信息的一个请求对象
+    :return: response 响应对象
+    """
+    return HttpResponse("hello world")
+
+
+def about(request):
+    return HttpResponse("这是一个about页面")
+
+
+def urltest(request,num):
+    print (num)
+    return HttpResponse("这是一个url测试视图%s"%(num))
+
+
+def urltestnew(request,city,year):
+
+
+    return HttpResponse("%s年在%s" %(year,city))
